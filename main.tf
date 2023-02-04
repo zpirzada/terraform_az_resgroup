b4f32dc1111e2f0679641e143d077750e52f3466
Zeb Pirzada <zpirzada@gmail.com>
2023-02-05 00:46:47 +0100

    main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.41.0"
+    }
+  }
+
+  required_version = ">= 0.15"
+}
+provider "azurerm" {
+  features{}
+}
+
+#Creates resource group
+resource "azurerm_resource_group" "main" {
+    name = "zebi-tf-rg-eastus"
+    location = "eastus"
+}
+
+output "out_msg" {
+        value = azurerm_resource_group.main.name
+}
